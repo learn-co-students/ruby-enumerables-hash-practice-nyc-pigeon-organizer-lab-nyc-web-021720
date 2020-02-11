208956fe4e288be28ef78899e17ce1cae8ab556e
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-11 20:25:20 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+hash = {}
+
+data.each do |colors_genders_lives,values|
+  values.each do |value,array|
+    array.each do |name|
+      if hash[name] == nil 
+        hash[name] = {}
+      end 
+      binding.pry 
+      if hash[name][colors_genders_lives] == nil 
+        hash[name][colors_genders_lives] = []
+      end 
+      
+      hash[name][colors_genders_lives].push(value.to_s)
+    end 
+  end
 end
+
+ 
+hash 
+
+
+
+
+end 
+
