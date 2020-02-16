75fc2b75e173be909b7e625894d314b5b5dafc7d
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-16 07:03:01 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+   new_hash = {}
+
+  data.each do |key,value| 
+    value.each do |descriptor, names|
+      names.each do |pigeon|
+
+        if !new_hash[pigeon]
+          new_hash[pigeon] = {} 
+        end
+
+        if !new_hash[pigeon][key]
+          new_hash[pigeon][key] =[]
+        end
+
+          new_hash[pigeon][key] << descriptor.to_s
+      end
+    end
+  end
+  new_hash
 end
